6c4b4deb70758257db30668ad363fbbf5d78c0d2
SJ <shajithtt786@gmail.com>
2026-05-13 23:18:54 +0530

    Add Vercel Analytics to track traffic

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import MainSite from './MainSite';
 import Admin from './Admin.tsx';
 import './index.css';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainSite />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainSite />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </Router>
+      <Analytics />
+    </>
   );
 }
